1184bdd739cf4349bec6e85a005ecf89f11195c2
Micah Martin <micah@8thlight.com>
2008-10-27 14:53:55 -0500

    restructured project to removed files from production.  Added rubydoc to player

--- battleship/spec/spec_helper.rb
+++ battleship/spec/spec_helper.rb
@@ -3,5 +3,5 @@ $: << File.expand_path(File.dirname(__FILE__) + "/../lib")
 
 $PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../")
 #require 'limelight/specs/spec_helper'
-require File.expand_path(File.dirname(__FILE__) + "/../../limelight/lib/limelight/specs/spec_helper")
+require File.expand_path(File.dirname(__FILE__) + "/../../../limelight/lib/limelight/specs/spec_helper")
 
